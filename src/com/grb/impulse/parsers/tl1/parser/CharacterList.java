745294f3e7a80d43ece94ae669a4b8a8b5643d2f
Bromfield <gbromfie@onm-gbromfie-01.ciena.com>
2015-11-17 14:29:37 -0500

    Added TextParser

---
+++ src/com/grb/impulse/parsers/tl1/parser/CharacterList.java
@@ -0,0 +1,244 @@
+package com.grb.impulse.parsers.tl1.parser;
+
+/**
+ * Created by gbromfie on 11/13/15.
+ */
+public class CharacterList {
+
+    public static final int NUMBER_CHARACTERS = 128;
+
+    public static final CharacterList ALL_CHARS = createAllList();
+    public static final CharacterList NO_CHARS = createNoList();
+    public static final CharacterList CONTROL_CHARS = createControlCharList();
+    public static final CharacterList ALPHABETIC_CHARS = createAlphabeticCharList();
+    public static final CharacterList ALPHABETIC_MINUS_WHITESPACE_CHARS = createAlphabeticMinusWhitespaceCharList();
+    public static final CharacterList WHITESPACE_CHARS = createWhitespaceCharList();
+    public static final CharacterList SPACE_TAB_CHARS = createSpaceTabCharList();
+    public static final CharacterList CR_LF_CHARS = createCRLFCharList();
+    public static final CharacterList NUMBER_CHARS = createNumberCharList();
+
+    private static CharacterList createAllList() {
+        boolean[] charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < NUMBER_CHARACTERS; i++) {
+            charList[i] = true;
+        }
+        return new CharacterList(charList, true);
+    }
+
+    private static CharacterList createNoList() {
+        boolean[] charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < NUMBER_CHARACTERS; i++) {
+            charList[i] = false;
+        }
+        return new CharacterList(charList, true);
+    }
+
+    private static CharacterList createControlCharList() {
+        boolean[] charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < 32; i++) {
+            charList[i] = true;
+        }
+        for(int i = 32; i < 127; i++) {
+            charList[i] = false;
+        }
+        charList[127] = true;
+        return new CharacterList(charList, true);
+    }
+
+    private static CharacterList createAlphabeticCharList() {
+        boolean[] charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < 32; i++) {
+            charList[i] = false;
+        }
+        for(int i = 32; i < 127; i++) {
+            charList[i] = true;
+        }
+        charList[127] = false;
+        return new CharacterList(charList, true);
+    }
+
+    private static CharacterList createNumberCharList() {
+        boolean[] charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < NUMBER_CHARACTERS; i++) {
+            charList[i] = false;
+        }
+        for(int i = '0'; i <= '9'; i++) {
+            charList[i] = true;
+        }
+        return new CharacterList(charList, true);
+    }
+
+    private static CharacterList createAlphabeticMinusWhitespaceCharList() {
+        boolean[] charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < 32; i++) {
+            charList[i] = false;
+        }
+        for(int i = 32; i < 127; i++) {
+            charList[i] = true;
+        }
+        charList[127] = false;
+        charList[' '] = false;
+        return new CharacterList(charList, true);
+    }
+
+    private static CharacterList createWhitespaceCharList() {
+        boolean[] charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < NUMBER_CHARACTERS; i++) {
+            charList[i] = false;
+        }
+        charList[9] = true;
+        charList[10] = true;
+        charList[11] = true;
+        charList[12] = true;
+        charList[13] = true;
+        charList[32] = true;
+        return new CharacterList(charList, true);
+    }
+
+    private static CharacterList createSpaceTabCharList() {
+        boolean[] charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < NUMBER_CHARACTERS; i++) {
+            charList[i] = false;
+        }
+        charList[9] = true;
+        charList[32] = true;
+        return new CharacterList(charList, true);
+    }
+
+    private static CharacterList createCRLFCharList() {
+        boolean[] charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < NUMBER_CHARACTERS; i++) {
+            charList[i] = false;
+        }
+        charList[10] = true;
+        charList[13] = true;
+        return new CharacterList(charList, true);
+    }
+
+    private static final String[] ControlCodeStrs = {
+            "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS", "TAB", "LF", "VT", "FF", "CR", "SO",
+            "SI", "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US"
+    };
+
+    public static final String[] CHARACTER_STRINGS = createCodes();
+
+    private static String[] createCodes() {
+        String[] codes = new String[NUMBER_CHARACTERS];
+        for(int i = 0; i < 32; i++) {
+            codes[i] = ControlCodeStrs[i];
+        }
+        for(int i = 32; i < 127; i++) {
+            codes[i] = String.valueOf((char)i);
+        }
+        codes[127] = "DEL";
+        return codes;
+    }
+
+    private boolean[] _charList;
+    private boolean _readOnly;
+
+    public CharacterList() {
+        this(NO_CHARS, false);
+    }
+
+    public CharacterList(CharacterList charList) {
+        this(charList._charList, false);
+    }
+
+    public CharacterList(CharacterList charList, boolean readOnly) {
+        this(charList._charList, readOnly);
+    }
+
+    public CharacterList(boolean[] charList) {
+        this(charList, false);
+    }
+
+    public CharacterList(boolean[] charList, boolean readOnly) {
+        if ((charList == null) || (charList.length != NUMBER_CHARACTERS)) {
+            throw new IllegalArgumentException("CharList must be of size " + NUMBER_CHARACTERS);
+        }
+        _charList = new boolean[NUMBER_CHARACTERS];
+        for(int i = 0; i < NUMBER_CHARACTERS; i++) {
+            _charList[i] = charList[i];
+        }
+        _readOnly = readOnly;
+    }
+
+    public CharacterList add(CharacterList charList) {
+        checkReadOnly();
+        for (int i = 0; i < NUMBER_CHARACTERS; i++) {
+            if (charList._charList[i]) {
+                _charList[i] = true;
+            }
+        }
+        return this;
+    }
+
+    public CharacterList add(char c) {
+        checkReadOnly();
+        _charList[c] = true;
+        return this;
+    }
+
+    public CharacterList remove(CharacterList charList) {
+        checkReadOnly();
+        for (int i = 0; i < NUMBER_CHARACTERS; i++) {
+            if (charList._charList[i]) {
+                _charList[i] = false;
+            }
+        }
+        return this;
+    }
+
+    public CharacterList remove(char c) {
+        checkReadOnly();
+        _charList[c] = false;
+        return this;
+    }
+
+    public CharacterList set(CharacterList charList) {
+        checkReadOnly();
+        for (int i = 0; i < NUMBER_CHARACTERS; i++) {
+            _charList[i] = charList.matches((byte)i);
+        }
+        return this;
+    }
+
+    public boolean matches(char c) {
+        return _charList[c];
+    }
+
+    public boolean matches(byte b) {
+        return _charList[b];
+    }
+
+    public int getSize() {
+        int count = 0;
+        for(int i = 0; i < NUMBER_CHARACTERS; i++) {
+            if (_charList[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String toString() {
+        StringBuilder bldr = new StringBuilder("[");
+        for(int i = 0; i < NUMBER_CHARACTERS; i++) {
+            if (_charList[i]) {
+                if ((bldr.length() > 1) && (i < 32)) {
+                    bldr.append(",");
+                }
+                bldr.append(CHARACTER_STRINGS[i]);
+            }
+        }
+        bldr.append("]");
+        return bldr.toString();
+    }
+
+    private void checkReadOnly() {
+        if (_readOnly) {
+            throw new IllegalStateException("This object is read only");
+        }
+    }
+}
---
+++ src/com/grb/impulse/parsers/tl1/parser/TextParser.java
@@ -0,0 +1,160 @@
+package com.grb.impulse.parsers.tl1.parser;
+
+import java.text.ParseException;
+
+/**
+ * Created by gbromfie on 11/16/15.
+ */
+public class TextParser {
+
+    private byte[] _beginDelimiter;
+    private byte[] _endDelimiter;
+    private boolean _includeDelimiter;
+    private CharacterList _allowedChars;
+    private CharacterList _delimiterChars;
+    private int _minLength;
+    private int _maxLength;
+
+    public TextParser() {
+        _beginDelimiter = null;
+        _endDelimiter = null;
+        _includeDelimiter = true;
+        _allowedChars = new CharacterList(CharacterList.ALL_CHARS);
+        _delimiterChars = new CharacterList(CharacterList.NO_CHARS);
+        _minLength = 0;
+        _maxLength = Integer.MAX_VALUE;
+    }
+
+    public TextParser addDelimeterChars(CharacterList delimiterChars) {
+        _delimiterChars.add(delimiterChars);
+        return this;
+    }
+
+    public TextParser removeDelimeterChars(CharacterList delimiterChars) {
+        _delimiterChars.remove(delimiterChars);
+        return this;
+    }
+
+    public TextParser setDelimiterChars(CharacterList delimiterChars) {
+        _delimiterChars.set(delimiterChars);
+        return this;
+    }
+
+    public CharacterList getDelimiterChars() {
+        return _delimiterChars;
+    }
+
+    public TextParser setDelimiterStrings(String begin, String end) {
+        _beginDelimiter = begin.getBytes();
+        _endDelimiter = end.getBytes();
+        return this;
+    }
+
+    public TextParser includeDelimiter(boolean includeDelimiter) {
+        _includeDelimiter = includeDelimiter;
+        return this;
+    }
+
+    public TextParser addAllowedChars(CharacterList allowedChars) {
+        _allowedChars.add(allowedChars);
+        return this;
+    }
+
+    public TextParser setAllowedChars(CharacterList allowedChars) {
+        _allowedChars.set(allowedChars);
+        return this;
+    }
+
+    public TextParser removeAllowedChars(CharacterList allowedChars) {
+        _allowedChars.remove(allowedChars);
+        return this;
+    }
+
+    public CharacterList getAllowedChars() {
+        return _allowedChars;
+    }
+
+    public TextParser setLengths(int min, int max) {
+        _minLength = min;
+        _maxLength = max;
+        return this;
+    }
+
+    public String parse(ParseContext ctx) throws ParseException {
+        return parse(ctx, Integer.MAX_VALUE);
+    }
+
+    public String parse(ParseContext ctx, Integer length) throws ParseException {
+        int offset = ctx.index;
+        StringBuilder bldr = new StringBuilder();
+        if (_beginDelimiter != null) {
+            if ((ctx.buffer.length - ctx.index) >= _beginDelimiter.length) {
+                for(int i = 0; i < _beginDelimiter.length; i++) {
+                    if (_beginDelimiter[i] != ctx.buffer[ctx.index]) {
+                        throw new ParseException("No Delimiter Found", ctx.index);
+                    }
+                    if (_includeDelimiter) {
+                        bldr.append((char)(ctx.buffer[ctx.index]));
+                        if (bldr.length() == length) {
+                            ctx.index = ctx.index + 1;
+                            validateLength(bldr.length());
+                            return bldr.toString();
+                        }
+                    }
+                    ctx.index = ctx.index + 1;
+                }
+            }
+        }
+        while(ctx.index < ctx.buffer.length) {
+            if (_endDelimiter != null) {
+                boolean found = true;
+                if ((ctx.index - offset + 1) >= _endDelimiter.length) {
+                    for (int i = 0; i < _endDelimiter.length; i++) {
+                        if (ctx.buffer[ctx.index - i] != _endDelimiter[_endDelimiter.length - i - 1]) {
+                            found = false;
+                            break;
+                        }
+                    }
+                    if (found) {
+                        if (_includeDelimiter) {
+                            bldr.append((char)(ctx.buffer[ctx.index]));
+                        } else {
+                            bldr.setLength(bldr.length() - _endDelimiter.length + 1);
+                        }
+                        ctx.index = ctx.index + 1;
+                        validateLength(bldr.length());
+                        return bldr.toString();
+                    }
+                }
+            }
+            if (_delimiterChars.matches(ctx.buffer[ctx.index])) {
+                if (_includeDelimiter) {
+                    bldr.append((char)(ctx.buffer[ctx.index]));
+                }
+                validateLength(bldr.length());
+                return bldr.toString();
+            }
+            if (!_allowedChars.matches(ctx.buffer[ctx.index])) {
+                throw new ParseException(
+                        String.format("Character \"%s\" is not allowed, must be one of %s",
+                                CharacterList.CHARACTER_STRINGS[ctx.buffer[ctx.index]], _allowedChars), ctx.index);
+            }
+            bldr.append((char)(ctx.buffer[ctx.index]));
+            ctx.index = ctx.index + 1;
+            if (bldr.length() == length) {
+                validateLength(bldr.length());
+                return bldr.toString();
+            }
+        }
+        return bldr.toString();
+    }
+
+    private void validateLength(int length) throws ParseException {
+        if (length < _minLength) {
+            throw new ParseException(String.format("Size of %d does not meet minimum size of %d", length, _minLength), length);
+        }
+        if (length > _maxLength) {
+            throw new ParseException(String.format("Size of %d exceeds maximum size of %d", length, _maxLength), length);
+        }
+    }
+}
