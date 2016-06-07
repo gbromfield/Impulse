56fd5d0e696ea5091762857db69b5ef49b84c824
Graham Bromfield <gbromfie@ciena.com>
2016-06-07 16:16:26 -0400

    Added Delay, Drop and Deny configs

--- config/tl1Error.js
+++ config/tl1Error.js
@@ -1,10 +1,35 @@
+/**
+ * Prints the Connection Context.
+ *
+ * @param connectionContext ConnectionContext to print.
+ * @returns {*}
+ */
+function printConnectionContext(logger, connection, argMap, args) {
+    logger.info('connection=' + connection);
+    logger.info('argMap=' + argMap);
+    if (args != null) {
+        var i = 0;
+        while(i < args.length) {
+            logger.info('args[' + i + "]= " + args[i]);
+            i++;
+        }
+    }
+    /**
+     * Returning false cancels the following of this connection.
+     */
+    return true;
+}
+
 function sendTL1Error(logger, connection, argMap, args) {
     printConnectionContext(logger, connection, argMap, args);
     var String = Java.type("java.lang.String");
-    var response = new String("\r\n\n " + args[5].getTid() + " " + args[5].getDate() + " " + args[5].getTime() + "\r\nM  " + args[5].getCTAG() + " DENY;");
+    tl1Message = argMap.get("TL1AgentFramer.TL1Message.tl1Message");
+    var response = new String("\r\n\n   \"" + tl1Message.getTid() + "\" " + tl1Message.getDate() +
+        " " + tl1Message.getTime() + "\r\nM  " + tl1Message.getCTAG() +
+        " DENY\r\n   SARB\r\n   /* Out To Lunch */\r\n;");
     var ByteBuffer = Java.type("java.nio.ByteBuffer");
     var buffer = ByteBuffer.allocate(100);
     buffer.put(response.getBytes());
     buffer.flip();
-    args[1] = buffer;
+    argMap.put("TL1AgentFramer.ByteBuffer.bytesTL1Message", buffer);
 }
